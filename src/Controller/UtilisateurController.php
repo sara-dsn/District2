699685898be3ac3528d415815c29a9b0586ee0b8
sara-dsn <sabr.tdg@gmail.com>
2024-02-16 11:26:16 +0100

    i

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,22 +2,46 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UtilisateurController extends AbstractController
 {
     #[Route('/connection', name: 'app_connection')]
-    public function connection(): Response
+    public function connection(EntityManagerInterface $manager): Response
     {
+        
+        $form=$this->createForm(UserFormType::class);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user =new User();
+            $data = $form->getData();
+            $user =$data;
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('app_accueil');
+        }
+
         return $this->render('utilisateur/connection.html.twig', [
+            'formulaire' =>$form 
         ]);
     }
+
+
     #[Route('/panier', name: 'app_panier')]
     public function panier(): Response
     {
         return $this->render('utilisateur/panier.html.twig', [
         ]);
     }
+    // public function index(UserPasswordHasher $passwordHasher):Response 
+    // {
+
+    // }
 }
