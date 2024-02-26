c9268e7cc1c96a06e01d5ce1b42698c5f90b6437
sara-dsn <sabr.tdg@gmail.com>
2024-02-26 10:54:41 +0100

    h

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -20,9 +20,9 @@ class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function accueil(): Response
     {
-        $categorie=$this->categorieRepo->findBy( [], null ,$limit='6');
-        $plat=$this->platRepo->findBy( [], null ,$limit='3');
-        $platsm=$this->platRepo->findBy( [], null ,$limit='6');
+        $categorie=$this->categorieRepo->findBy( ['active'=>1], null ,$limit='6');
+        $plat=$this->platRepo->findBy( ['active'=>1], null ,$limit='3');
+        $platsm=$this->platRepo->findBy( ['active'=>1], null ,$limit='6');
 
         return $this->render('accueil/accueil.html.twig', [
             'categorie'=>$categorie,
@@ -34,7 +34,7 @@ class AccueilController extends AbstractController
     #[Route('/categorie', name: 'app_categorie')]
     public function categorie(): Response
     {
-        $categorie=$this->categorieRepo->findBy( [], null ,$limit='6');
+        $categorie=$this->categorieRepo->findBy( ['active'=>1], null ,$limit='6');
 
         return $this->render('accueil/categorie.html.twig', [
         'categorie'=>$categorie
@@ -43,7 +43,7 @@ class AccueilController extends AbstractController
     #[Route('/plat', name: 'app_plat')]
     public function plat(): Response
     {
-        $plt=$this->platRepo->findBy( [], null ,$limit='6');
+        $plt=$this->platRepo->findBy( ['active'=>1], null ,$limit='6');
         return $this->render('accueil/plat.html.twig', [
             'plat'=>$plt
         ]);
